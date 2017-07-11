64d540fe80b6e13cc871d30d98377dbf950e27f9
Dylan <dylan.patrick.cheng@gmail.com>
2017-07-11 16:33:00 -0700

    build skeleton of PostIndex and CommentIndex classes

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import PostIndex from './post_index';
+import CommentIndex from './comment_index';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: "" };
+    this.state = { user: "", posts: {}, comments: {} };
   }
 
   handleChange(e) {
@@ -13,7 +16,12 @@ class App extends React.Component {
   render() {
     return (
       <main>
-        <input onChange={ this.handleChange.bind(this) }></input>
+        <label>
+          Username: <input onChange={ this.handleChange.bind(this) }></input>
+        </label>
+        <button>Search</button>
+        <PostIndex posts={ this.state.posts }/>
+        <CommentIndex comments={ this.state.comments }/>
       </main>
     );
   }
---
+++ frontend/post_index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PostIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main>
+        posts
+      </main>
+    );
+  }
+}
+
+export default PostIndex;
