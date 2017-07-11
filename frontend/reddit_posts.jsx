71822d37f2e1be4f958c9cd20b475499dfa390d0
Dylan <dylan.patrick.cheng@gmail.com>
2017-07-11 16:21:24 -0700

    set up entry file

---
+++ frontend/reddit_posts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => {
+    return (
+      <main>
+        
+      </main>
+    );
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root />, root);
+});
